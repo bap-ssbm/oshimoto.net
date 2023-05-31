9dfb0d776f688cc84ac8f6467157060026f20e7f
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-05-31 12:10:44 +0900

    Add files via upload

--- app/components/about/Galleries.tsx
+++ app/components/about/Galleries.tsx
@@ -11,7 +11,7 @@ const Galleries: FC<GalleriesProps> = ({ english }) => {
     return (
         <div className='flex flex-col gap-10 md:w-[50%]'>
             <div>
-                <h1 className='font-Nixie-One tracking-[4px]'>-Galleries-</h1>
+                <h1 className='font-Nixie-One tracking-[4px]'>-Exhibitions-</h1>
             </div>
             <ul className='text-[14px] space-y-5'>
                 <li>
@@ -41,20 +41,38 @@ const Galleries: FC<GalleriesProps> = ({ english }) => {
                 <li>
                     <div className='space-x-4'>
                         <p className='text-[14px]'>
+                            SPACE AO in Hayama
+                        </p>
+                        <p>
+                        Death valley
+                        </p>
+                    </div>
+                </li>
+
+                <li>
+                    <div className='space-x-4'>
+                        <p className='text-[14px]'>
                             LUMIX BASE TOKYO
                         </p>
                         <AnimatePresence mode='wait'>
-                        {english?
-                        (<motion.p
-                            key={1}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1, transition: { duration: 0.4 } }}
-                            exit={{ opacity: 0 }}> Alegría: In A New Light</motion.p>):
-                        (<motion.p
-                            key={2}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1, transition: { duration: 0.4 } }}
-                            exit={{ opacity: 0 }}>「新たなる光 生まれ変わったアレグリア」</motion.p>)}
+                            {english ?
+                                (<motion.p
+                                    key={1}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1, transition: { duration: 0.4 } }}
+                                    exit={{ opacity: 0 }}> Alegría: In A New Light
+                                </motion.p>) :
+                                (<motion.p
+                                    key={2}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1, transition: { duration: 0.4 } }}
+                                    exit={{ opacity: 0 }}>「新たなる光 生まれ変わったアレグリア」</motion.p>)}
+                            <a className='underline ml-3' href='/events/lbtsyasinten.pdf'>
+                                PDF 1
+                            </a>
+                            <a className='underline ml-3' href='/events/LBTphoto.oshimoto.pdf'>
+                                PDF 2
+                            </a>
                         </AnimatePresence>
                     </div>
                 </li>
