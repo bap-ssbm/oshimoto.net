2055afddb716e7a1c6b4b684a96de4adc543e2e4
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-06-04 17:52:22 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-  images: {
-    domains: [
-      'drive.google.com',
-      "lh3.googleusercontent.com"
-    ],
-  },
+    images: {
+        domains: [
+          'drive.google.com',
+          "lh3.googleusercontent.com"
+        ],
+      },
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontFamily: {
-      'Nixie-One': ['Nixie One', 'cursive'],
-      'Raleway': ['Raleway', 'sans-serif'],
-    },
     extend: {
+      fontFamily:{
+        'AnonymousPro':['AnonymousPro', 'sans-serif'],
+        'AbrilFatFace':['AbrilFatFace', 'sans-serif'],
+        'BrandonGrotesque':['BrandonGrotesque', 'sans-serif'],
+        'Geologica':['Geologica', 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+        'black-950':'#111216',
+        'black-900':'#171717',
+        'black-800':'#252525',
+        'black-700':'#333333',
+        'black-600':'#FDF2F2',
+        'black-500':'#F4F5F7',
+        'black-400':'#929292'
       },
     },
   },
